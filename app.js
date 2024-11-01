3a327a3f21f64eec0c9419bd1462b0e44f0e0322
yasu-git <yasu.git.33@gmail.com>
2024-11-01 22:15:41 +0900

    add hello page

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
+//必要なモジュールの読み込み
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//ルート用モジュールの読み込み
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 
+//expressオブジェクトの生成
 var app = express();
 
+//基本設定
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//関数の組み込み
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//ルート、エラー処理の設定
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +46,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//model.exportsでappオブジェクトをモジュールとして公開
 module.exports = app;
