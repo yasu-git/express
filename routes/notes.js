312955fddb1b55a4d6c94def7812cc51c40fd731
yasu-git <yasu.git.33@gmail.com>
2024-11-22 08:39:35 +0900

    add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,39 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-//レスポンスのデータ(ノートの全権)
-const resposeObjectDataAll = {
-	/**object配列
-	*id
-	*title
-	*subTitle
-	*bodyText
-	**/
-	textObject1: {
-		id: 1,
-		title: 'ノート１のタイトルです。',
-		subTitle: 'ノート１のサブタイトルです。',
-		bodyText: 'ノート１の本文です。',
-	},
-	textObject2: {
-		id: 2,
-		title: 'ノート２のタイトルです。',
-		subTitle: 'ノート２のサブタイトルです。',
-		bodyText: 'ノート２の本文です。',
-	},
-};
+//接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "******";
+const client = new MongoClient(uri);
 
-/**
- * メモを全権取得するapi
- * @param {Object[]} data
- * @param {number} data.id - ID
- * @param {string} data.title - タイトル
- * @param {string} data.text - 内容
- */
+router.get('/', async(req, res) => {
+	//データベース、コレクションを取得
+	const database = client.db('notes');
+	const notes = database.collection('notes');
 
-router.get('/', function(req, res, next) {
-	//全権取得して返す
-	res.json(resposeObjectDataAll);
+	//idが1のドキュメントを更新
+	const query = { id: 1};
+	const note =  await notes.findOne(query);
+
+	res.json(note);
 });
 
 module.exports = router;
