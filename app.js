7e52c207a20be0072b1ccbb273b6f9eed85cd382
yasu-git <yasu.git.33@gmail.com>
2024-11-23 08:00:45 +0900

    add cat.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
 
 //expressオブジェクトの生成
 var app = express();
@@ -31,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes',notesRouter);
+app.use('/cat',catRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
